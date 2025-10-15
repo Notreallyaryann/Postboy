3f9725637839b6657e1f159ceaefda9a664daf92
Aryan <vedprakashverma4842@gmail.com>
2025-10-15 10:27:53 +0530

    fixed auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,10 @@
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://postman-clone-gamma.vercel.app"
+      : "http://localhost:3000",
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession } = authClient
